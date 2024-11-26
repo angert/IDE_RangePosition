ca9de0e0d01cfe6f8885186e9de54cd929ca2650
Allison Louthan <104224011+allisonmlouthan@users.noreply.github.com>
2024-11-26 12:08:14 -0600

    Update 01_prep_input_data.R

--- scripts/01_prep_input_data.R
+++ scripts/01_prep_input_data.R
@@ -1,12 +1,11 @@
 # project: IDE (international drought experiment)
 # code summary: get data prepared to calc position within range or niche for IDE sites
 # written by: A Louthan
-# modified: 241107
+# modified: 241121
 
 # libraries and functions -----
 library(tidyr)
 library(dplyr)
-setwd("/Users/allisonlouthan/Dropbox/climate across latitudes/IDE/IDE_RangePosition")
 
 # load global info data-----
 r <- raster::stack(
@@ -19,8 +18,7 @@ res <-prcomp(r.matrix, scale = TRUE, retx=TRUE) # run a PCA on the climate data;
 means.indices <- c(1, 5, 6, 8, 9, 10, 11, 12, 13, 14, 16, 17, 18, 19) 
 r.matrix.means <- r.matrix[,means.indices]
 res.means <-prcomp(r.matrix.means, scale = TRUE, retx=TRUE) # calc a global PCA with only mean-associated bioclim variables
-
-rm(r.matrix, r.matrix.means) # remove these giant files
+rm(r.matrix, r.matrix.means,r) # remove these giant files
 
 # which species are at which site?
 species_site_counts <- read.csv("data/species_site_consecyears.csv")
@@ -50,9 +48,8 @@ colnames(species_sites) <- c("species","site")
 # load IDE site locations into species_sites file
 site_info <- read.csv("data/Site_Elev-Disturb241104.csv")[,c("site_code", "latitud", "longitud")]
 colnames(site_info) <- c("site", "Lat", "Long")
-left_join(species_sites, site_info, by= "site")
+species_sites <- left_join(species_sites, site_info, by= "site")
 # IDE metadata says: Latitude	== latitude from -90 (S) to +90 (N) in decimal degrees	
 # Longitude	== longitude from -180 (W) to +180 (E) in decimal degrees
-
 
 save.image(file= "data/01_input_data.RData")
